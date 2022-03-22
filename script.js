410606290ee6f3c98207cd6775bbee5f37287aea
TalelKarim <talelchebbi090@gmail.com>
2022-03-23 00:32:45 +0100

    final commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let time = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let milliseconds =0;
 
 let start = document.querySelector('.start');
 let stop = document.querySelector('.stop');
@@ -11,14 +12,17 @@ let reset = document.querySelector('.reset');
 
 let heures = document.querySelector('.hours');
 let minutess = document.querySelector('.minutes');
-let secondes = document.querySelector('.seconds')
-
+let secondes = document.querySelector('.seconds');
+let millisecondes = document.querySelector('.milliseconds');
 let demarrage = null;
+
+
 // Fonction d'affichage du temps
 function showTime(time){
-     hours = Math.floor(time / 3600) ;
-     minutes = Math.floor((time % 3600) / 60 );
-     seconds = time % 60;
+     hours = Math.floor(time / 3600000) ;
+     minutes = Math.floor((time % 3600000) / 60000 );
+     seconds = Math.floor((time % 60000 )/ 1000);
+     milliseconds = time % 1000;
      
      
      if (hours < 10){
@@ -33,15 +37,26 @@ function showTime(time){
         seconds = "0" + seconds;
     }
 
+    if (milliseconds < 100){
+        if(milliseconds < 10){
+            milliseconds = "00" + milliseconds;
+            }
+        if(milliseconds >= 10){
+            milliseconds = "0" + milliseconds;
+        }    
+        
+    }
+  
+  
     heures.innerText = hours;
     minutess.textContent = minutes;
     secondes.textContent = seconds;
-
+    millisecondes.textContent = milliseconds;
 }
 
 showTime(0);
 function incrementTime(){
-    time += 1;
+    time += 50;
     showTime(time);
 }
 
@@ -49,7 +64,7 @@ function incrementTime(){
 
 start.addEventListener('click', function(){
            start.disabled = "true";
-           demarrage =  setInterval(incrementTime,1000);
+           demarrage =  setInterval(incrementTime,50);
            
     
 });
